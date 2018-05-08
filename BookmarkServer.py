c8bc0906029b9594d9a8ff6882f248d433390eed
Nick Cassady <nickcassady7@gmail.com>
2018-05-08 09:03:32 -0400

    Actually pushing the previous commit, to change error messgaes

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -1,9 +1,49 @@
 #!/usr/bin/env python3
 #
-# A *bookmark server* or URI shortener.
+# A *bookmark server* or URI shortener that maintains a mapping (dictionary)
+# between short names and long URIs, checking that each new URI added to the
+# mapping actually works (i.e. returns a 200 OK).
+#
+# This server is intended to serve three kinds of requests:
+#
+#   * A GET request to the / (root) path.  The server returns a form allowing
+#     the user to submit a new name/URI pairing.  The form also includes a
+#     listing of all the known pairings.
+#   * A POST request containing "longuri" and "shortname" fields.  The server
+#     checks that the URI is valid (by requesting it), and if so, stores the
+#     mapping from shortname to longuri in its dictionary.  The server then
+#     redirects back to the root path.
+#   * A GET request whose path contains a short name.  The server looks up
+#     that short name in its dictionary and redirects to the corresponding
+#     long URI.
+#
+# Your job in this exercise is to finish the server code.
+#
+# Here are the steps you need to complete:
+#
+# 1. Write the CheckURI function, which takes a URI and returns True if a
+#    request to that URI returns a 200 OK, and False otherwise.
+#
+# 2. Write the code inside do_GET that sends a 303 redirect to a known name.
+#
+# 3. Write the code inside do_POST that sends a 400 error if the form fields
+#    are missing.
+#
+# 4. Write the code inside do_POST that sends a 303 redirect to the form
+#    after saving a newly submitted URI.
+#
+# 5. Write the code inside do_POST that sends a 404 error if a URI is not
+#    successfully checked (i.e. if CheckURI returns false).
+#
+# In each step, you'll need to delete a line of code that raises the
+# NotImplementedError exception.  These are there as placeholders in the
+# starter code.
+#
+# After writing each step, restart the server and run test.py to test it.
 
 import http.server
 import requests
+import os
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -35,12 +75,13 @@ def CheckURI(uri, timeout=5):
     False if that GET request returns any other response, or doesn't return
     (i.e. times out).
     '''
+    # 1. Write this function.  Delete the following line.
     try:
         r = requests.get(uri, timeout=timeout)
-        # If the GET request returns, was it a 200 OK?
+        # Did the GET request return 200?
         return r.status_code == 200
     except requests.RequestException:
-        # If the GET request raised an exception, it's not OK.
+        # If the GET didn't get a 200, it's bad...
         return False
 
 
@@ -52,7 +93,8 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
         if name:
             if name in memory:
-                # We know that name! Send a redirect to it.
+                # 2. Send a 303 redirect to the long URI in memory[name].
+                # Send a redirect to the known name
                 self.send_response(303)
                 self.send_header('Location', memory[name])
                 self.end_headers()
@@ -84,7 +126,6 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
             self.wfile.write("Missing form fields!".encode())
-            return
 
         longuri = params["longuri"][0]
         shortname = params["shortname"][0]
@@ -93,9 +134,9 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             # This URI is good!  Remember it under the specified name.
             memory[shortname] = longuri
 
-            # Serve a redirect to the form.
+            # Redirect to the form
             self.send_response(303)
-            self.send_header('Location', '/')
+            self.send_header('location', '/')
             self.end_headers()
         else:
             # Didn't successfully fetch the long URI.
@@ -106,6 +147,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
